ae27b50e195610caf7c67f61c08025959be5ca16
donghyeon505 <ehdgus458@gmail.com>
2025-07-16 14:47:56 +0900

    feat: 필수 구현 과제 Lv2 - 생성자 및 게터 메서드 구현

---
+++ src/main/java/Kiosk/MenuItem.java
@@ -0,0 +1,27 @@
+package Kiosk;
+
+public class MenuItem {
+    private final String name;
+    private final String price;
+    private final String explain;
+
+    // 생성자
+    public MenuItem(String name, String price, String explain) {
+        this.name = name;
+        this.price = price;
+        this.explain = explain;
+    }
+
+    // 게터 메서드
+    public String getName() {
+        return name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public String getExplain() {
+        return explain;
+    }
+}
