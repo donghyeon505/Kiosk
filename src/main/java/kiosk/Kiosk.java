5530b71a4f3fa18c76a192137388050f477e3b43
donghyeon505 <ehdgus458@gmail.com>
2025-07-18 11:15:51 +0900

    feat: 장바구니 클래스 추가 - 가격 계산을 위해 price 타입을 Double로 변경 - MenuItem 게더메서드 복구 - Order Menu 출력 추가

--- src/main/java/kiosk/Kiosk.java
+++ src/main/java/kiosk/Kiosk.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 public class Kiosk {
     private final Scanner sc = new Scanner(System.in);
+    private final Shopping shopping = new Shopping();
     private final List<Menu> allMenus;
 
     private int featureNumber;
@@ -16,12 +17,28 @@ public class Kiosk {
     public void start() {
         // 선택한 메뉴 출력
         while (true) {
-            // 메뉴판
-                System.out.println("[  SHACKSHACK Burger  ]");
-                for (int i = 0; i < allMenus.size(); i++) {
-                    System.out.println("[" + (i + 1) + "] " + allMenus.get(i).getCategory());
-                }
-                System.out.println("[0] 종료");
+            // 메뉴판 번호용 변수
+            int MenuNumber = 0;
+
+            // 메뉴 상단
+            System.out.println("[  SHACKSHACK Burger  ]");
+
+            // 메뉴 출력
+            for (int i = 0; i < allMenus.size(); i++) {
+                System.out.println("[" + (i + 1) + "] " + allMenus.get(i).getCategory());
+                MenuNumber = i + 1;
+            }
+
+            // 메뉴 하단
+            System.out.println("[0] 종료\n");
+
+            // 메뉴 선택시 나오는 Order Menu
+            List<MenuItem> shoppingList = shopping.getShop();
+            if (!shoppingList.isEmpty()) {
+                System.out.println("[  Order Menu  ]");
+                System.out.println("[" + (MenuNumber + 1) + "] Orders  | 장바구니를 확인 후 주문합니다.");
+                System.out.println("[" + (MenuNumber + 2) + "] Cancel  | 진행중인 주문을 취소합니다.");
+            }
 
             try {
                 featureNumber = sc.nextInt();
@@ -31,7 +48,7 @@ public class Kiosk {
                     break;
                 }
                 viewMenu();
-            } catch(Exception e) {
+            } catch (Exception e) {
                 System.out.println("잘못된 입력입니다.");
                 sc.nextLine();
             }
@@ -41,7 +58,7 @@ public class Kiosk {
     public void viewMenu() {
         if (featureNumber >= 1 && featureNumber <= allMenus.size()) {
             Menu select = allMenus.get(featureNumber - 1);
-            System.out.println("\n[  " + select.getCategory() + "  ]");
+            System.out.println("[  " + select.getCategory() + "  ]");
             select.printMenu();
 
             int selectNumber = sc.nextInt();
@@ -52,6 +69,7 @@ public class Kiosk {
             if (selectNumber >= 1 && selectNumber <= allMenus.size()) {
                 MenuItem selectMenu = select.getMenu(selectNumber - 1);
                 System.out.println("선택한 메뉴: " + selectMenu + "\n");
+                shopping.addShop(selectMenu);
             } else {
                 System.out.println("잘못된 입력입니다.");
             }
--- src/main/java/kiosk/MenuItem.java
+++ src/main/java/kiosk/MenuItem.java
@@ -3,18 +3,30 @@ package kiosk;
 public class MenuItem {
 
     private final String name;
-    private final String price;
+    private final Double price;
     private final String explain;
 
     // 생성자
-    public MenuItem(String name, String price, String explain) {
+    public MenuItem(String name, Double price, String explain) {
         this.name = name;
         this.price = price;
         this.explain = explain;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public String getExplain() {
+        return explain;
+    }
+
     @Override
     public String toString() {
-        return name + " | " + price + " | " + explain;
+        return name + " | W " + price + " | " + explain;
     }
 }
